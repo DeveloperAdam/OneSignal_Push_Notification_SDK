419fae4348b815c10652eaa623319a57ea2cb98a
DeveloperAdam <adamnooriit@gmail.com>
2017-12-05 13:55:53 +0500

    One Signal
    Push notificatoin

---
+++ app/src/main/java/com/example/adamnoor/peshfd/MyApplication.java
@@ -0,0 +1,31 @@
+package com.example.adamnoor.peshfd;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.onesignal.OneSignal;
+
+/**
+ * Created by Adam Noor on 21-Nov-17.
+ */
+
+public class MyApplication extends Application {
+
+    private static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        //MyNotificationOpenedHandler : This will be called when a notification is tapped on.
+        //MyNotificationReceivedHandler : This will be called when a notification is received while your app is running.
+        OneSignal.startInit(this)
+                .setNotificationOpenedHandler(new MyNotificationOpenedHandler())
+                .setNotificationReceivedHandler( new MyNotificationReceivedHandler() )
+                .init();
+    }
+}
